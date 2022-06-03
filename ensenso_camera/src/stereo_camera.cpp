59445c56a55cde92b941c4b3868e18cbfadb506e
Benjamin Thiemann <benjamin.thiemann@ensenso.de>
2022-06-03 10:53:32 +0200

    stereo_camera: Fix copy-publishing of projected point cloud

--- ensenso_camera/src/stereo_camera.cpp
+++ ensenso_camera/src/stereo_camera.cpp
@@ -1093,7 +1093,7 @@ void StereoCamera::onTelecentricProjection(ensenso_camera_msgs::TelecentricProje
 
       if (goal->publish_results)
       {
-        projectedPointCloudPublisher.publish(*pointCloud);
+        projectedPointCloudPublisher.publish(pointCloud);
       }
       if (goal->include_results_in_response)
       {
