d0ec4c4113344ee982d6f75225c28dee1359225a
Benjamin Thiemann <benjamin.thiemann@ensenso.de>
2022-07-12 19:12:25 +0200

    Include ROS2 in GitHub actions

---
+++ .github/scripts/install_external_dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install script for external dependencies such as the Esenso SDK.
+
+set -e
+
+# Requires ENSENSO_INSTALL and ENSENSO_SDK_VERSION to be set.
+sudo apt-get -y install dpkg wget
+wget -O /tmp/ensenso.deb https://download.ensenso.com/s/ensensosdk/download?files=ensenso-sdk-${ENSENSO_SDK_VERSION}-x64.deb
+sudo dpkg -i /tmp/ensenso.deb
+sudo apt-get install -f -y
+
+if [[ $ROS_VERSION -eq "2" ]]; then
+    sudo apt-get -y install libopencv-dev python3-opencv
+    sudo apt-get -y install ros-galactic-tf-transformations
+    sudo pip3 install transforms3d
+fi
