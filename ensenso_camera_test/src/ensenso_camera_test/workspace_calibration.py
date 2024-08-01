9d4f98c39ce25282215bdf153ca177eb94605d1b
Benjamin Thiemann <benjamin.thiemann@optonic.com>
2024-08-01 07:05:00 +0200

    Fix workspace calibration test to work with NxLib 4.0
    With NxLib 4.0 file cameras have a follow link mechanism. If the camera
    parameter "Capture/Follow/Link" is not set to "Disabled" after the
    workspace calibration has been executed, the new follow mechanism will
    use the cached (original) camera link instead of the new one from the
    workspace calibration.

--- ensenso_camera_test/src/ensenso_camera_test/workspace_calibration.py
+++ ensenso_camera_test/src/ensenso_camera_test/workspace_calibration.py
@@ -4,12 +4,15 @@ import unittest
 
 import ensenso_camera.ros2 as ros2py
 
+from ensenso_camera_msgs.msg import Parameter
+
 import ensenso_camera_test.ros2_testing as ros2py_testing
 
 from geometry_msgs.msg import Pose, Point, Quaternion
 
 CalibrateWorkspace = ros2py.import_action("ensenso_camera_msgs", "CalibrateWorkspace")
 LocatePattern = ros2py.import_action("ensenso_camera_msgs", "LocatePattern")
+SetParameter = ros2py.import_action("ensenso_camera_msgs", "SetParameter")
 
 tolerance = 0.001
 
@@ -19,7 +22,17 @@ class TestWorkspaceCalibration(unittest.TestCase):
         self.node = ros2py.create_node("test_workspace_calibration")
         self.calibration_client = ros2py.create_action_client(self.node, "calibrate_workspace", CalibrateWorkspace)
         self.locate_pattern_client = ros2py.create_action_client(self.node, "locate_pattern", LocatePattern)
+        self.set_parameter_client = ros2py.create_action_client(self.node, "set_parameter", SetParameter)
         ros2py.wait_for_server(self.node, self.locate_pattern_client)
+
+    def disable_follow_link(self):
+        parameter = Parameter(key=Parameter.FOLLOW_LINK, string_value="Disabled")
+        goal = SetParameter.Goal(parameters=[parameter])
+        response = ros2py.send_action_goal(self.node, self.set_parameter_client, goal)
+
+        result = response.get_result()
+        self.assertTrue(response.successful())
+        self.assertEqual(result.error.code, 0)
 
     def test_workspace_calibration(self):
         for _ in range(5):
@@ -40,6 +53,8 @@ class TestWorkspaceCalibration(unittest.TestCase):
             self.assertTrue(response.successful())
             self.assertEqual(result.error.code, 0)
             self.assertTrue(result.successful)
+
+            self.disable_follow_link()
 
             # Locate the pattern. It should now be at the random pose.
             response = ros2py.send_action_goal(self.node, self.locate_pattern_client, LocatePattern.Goal())
